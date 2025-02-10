5663e550b8db367ecb97a83cb1c4a439421fbb48
Hamza Khan <Hamza Khan@HAMZA>
2025-02-10 20:12:08 +0500

    Add project files.

---
+++ FormWithTagHelpers/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace FormWithTagHelpers.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+        public string Designation { get; set; }
+        public string Married { get; set; }
+        public string Descripstion { get; set; }
+    }
+
+    public enum Gender
+    {
+        MALE,Female
+    }
+}
---
+++ FormWithTagHelpers/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace FormWithTagHelpers.Models;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string Fathername { get; set; }
+    public string Class { get; set; }
+    public StdGender Gender { get; set; }
+    public string Married { get; set; }
+}
+public enum StdGender
+{
+    Male,Female 
+}
